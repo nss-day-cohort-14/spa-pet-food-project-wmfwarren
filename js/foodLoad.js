77038bb39fb40c9af1544679e03e389a33345ab2
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-03 13:24:04 -0500

    Both printing functions work now.

--- js/foodLoad.js
+++ js/foodLoad.js
@@ -10,36 +10,83 @@ function loadFoodRequest () { //load the JSON
 	console.log("This is the cat food", catFoods);
 
 	printDogFood();
+	printCatFood();
 }
 
 function printDogFood () { //print the dog food object
 	var dogPrintZone = document.getElementById("dogFood");
 	
 	for(let i = 0; i < dogFoods.dog_brands.length; i++)	{//main loop for printing head info
+		var brandName = dogFoods.dog_brands[i].name;
 		dogPrintZone.innerHTML += `
-		<h2>${dogFoods.dog_brands[i].name}</h2>
-		<h3>Type and Pricing</h3>
+		<h1>Dog Food</h1>
+		<h2 class="brandName">${brandName}</h2>
+		<h3>Types and Pricing</h3>
 		`
 		for(let j = 0; j < dogFoods.dog_brands[i].types.length; j++){//second loop to print type of food
+				var currentFoodType = dogFoods.dog_brands[i].types[j].type;
+				currentFoodType = currentFoodType.replace(/_/, ' '); //replace underscore with space
+	
+				currentFoodType = currentFoodType.charAt(0).toUpperCase() + currentFoodType.slice(1); //make first char uppercase
+				
 			dogPrintZone.innerHTML += `
-				<h4 class=typeOfFood">${dogFoods.dog_brands[i].types[j].type}</h4>
+				<h4 class=typeOfFood">${currentFoodType}</h4>
 			`
 			for(let k = 0; k < dogFoods.dog_brands[i].types[j].volumes.length; k++){//third loop to print pricing and other info
+				var volName = dogFoods.dog_brands[i].types[j].volumes[k].name;
+				var volPrice = dogFoods.dog_brands[i].types[j].volumes[k].price;
 				dogPrintZone.innerHTML += `
-					<p class="foodVolume">${dogFoods.dog_brands[i].types[j].volumes[k].name}</p>
-					<p class="priceForVolume">$${dogFoods.dog_brands[i].types[j].volumes[k].price}</p>
+					<p class="foodVolume">${volName}</p>
+					<p class="priceForVolume">$${volPrice}</p>
 				`
 			}
 		}
 	}
 }
 
-function printCatFood () { //print the cat food object
+function printCatFood () { //print the cat food object need second function because of breeds. 
 	var catPrintZone = document.getElementById("catFood");
+	
+	for(let i = 0; i < catFoods.cat_brands.length; i++)	{//main loop for printing head info
+		var brandName = catFoods.cat_brands[i].name;
+		catPrintZone.innerHTML += `
+		<h1> Cat Food</h1>
+		<h2 class="brandName">${brandName}</h2>
+		<h3>Breeds, Types, and Pricing</h3>
+		`
+			for(let j = 0; j < catFoods.cat_brands[i].breeds.length; j++){//second loop to print breeds of cats
+			var currentFoodBreeds = catFoods.cat_brands[i].breeds[j];
+			currentFoodBreeds = currentFoodBreeds.replace(/_/, ' '); //replace underscore with space
 
+			currentFoodBreeds = currentFoodBreeds.charAt(0).toUpperCase() + currentFoodBreeds.slice(1); //make first char uppercase
+			
+			
+			catPrintZone.innerHTML += `
+				<h4 class=foodForBreed">${currentFoodBreeds}</h4>
+			`
+			}
+		for(let l = 0; l < catFoods.cat_brands[i].types.length; l++ ){ //print the food types
+				var currentFoodType = catFoods.cat_brands[i].types[l].type;
+				currentFoodType = currentFoodType.replace(/_/, ' '); //replace underscore with space
+	
+				currentFoodType = currentFoodType.charAt(0).toUpperCase() + currentFoodType.slice(1); //make first char uppercase
+	
+			catPrintZone.innerHTML += `
+				<h4 class=typeOfFood">${currentFoodType}</h4>
+			`
+			for(let k = 0; k < catFoods.cat_brands[i].types[l].volumes.length; k++){//third loop to print pricing and other info
+				var volName = catFoods.cat_brands[i].types[l].volumes[k].name;
+				var volPrice = catFoods.cat_brands[i].types[l].volumes[k].price;
+				catPrintZone.innerHTML += `
+					<p class="foodVolume">${volName}</p>
+					<p class="priceForVolume">$${volPrice}</p>
+				`
+			}
+		}
+	}
 }
 
-requestDogFood.addEventListener("load", loadFoodRequest); //add a load listener to the requestDogFood 
+requestDogFood.addEventListener("load", loadFoodRequest); //add a load listener to the requestcatFood 
 requestCatFood.addEventListener("load", loadFoodRequest);
 
 requestDogFood.open("GET", "../json/dogFoods.json"); //give information on retrieving the data to foodRequest
