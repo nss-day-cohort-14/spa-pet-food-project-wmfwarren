c862f50dcceaf41522b955c30490b84c0b1df0d2
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-06-17 11:58:10 -0500

    DRYed out code by condensing 1 function and combining two others. got rid of JSON error. Need to fix print bug

--- js/foodLoad.js
+++ js/foodLoad.js
@@ -1,83 +1,51 @@
 var requestDogFood = new XMLHttpRequest();
 var requestCatFood = new XMLHttpRequest();
-var dogFoods = null; 
-var catFoods = null;
+var Foods = null;
 
-function loadFoodRequest () { //load the JSON
-	dogFoods = JSON.parse(requestDogFood.responseText);
-	catFoods = JSON.parse(requestCatFood.responseText);
-	console.log("This is the dog food", dogFoods);
-	console.log("This is the cat food", catFoods);
+function loadFoodRequest (event) { //load the JSON
+	Foods = JSON.parse(event.target.responseText);
 
-	printDogFood();
-	printCatFood();
+	printFood();
 }
 
-function printDogFood () { //print the dog food object
-	var dogPrintZone = document.getElementById("dogFood");
+function printFood () { //print the  food object need second function because of breeds. 
+	var PrintZone = document.getElementById("food");
 	
-	for(let i = 0; i < dogFoods.dog_brands.length; i++)	{//main loop for printing head info
-		var brandName = dogFoods.dog_brands[i].name;
-		dogPrintZone.innerHTML += `
-		<h1>Dog Food</h1>
-		<h2 class="brandName">${brandName}</h2>
-		<h3>Types and Pricing</h3>
-		`
-		for(let j = 0; j < dogFoods.dog_brands[i].types.length; j++){//second loop to print type of food
-			var currentFoodType = dogFoods.dog_brands[i].types[j].type;
-			currentFoodType = currentFoodType.replace(/_/, ' '); //replace underscore with space
+	for(let i = 0; i < Foods.brands.length; i++)	{//main loop for printing head info
+		var brandName = Foods.brands[i].name;
+		PrintZone.innerHTML += `
+		<h1>Food</h1>
+		<h2 class="brandName">${brandName}</h2>`;
 
-			currentFoodType = currentFoodType.charAt(0).toUpperCase() + currentFoodType.slice(1); //make first char uppercase
+		if (Foods.brands[i].breeds.length !== undefined) {
+			PrintZone.innerHTML += `<h3>Breeds</h3>`;
+			for(let j = 0; j < Foods.brands[i].breeds.length; j++){//second loop to print breeds of s
+			var currentFoodBreeds = Foods.brands[i].breeds[j];
+			currentFoodBreeds = currentFoodBreeds.replace(/_/, ' '); //replace underscore with space
+
+			currentFoodBreeds = currentFoodBreeds.charAt(0).toUpperCase() + currentFoodBreeds.slice(1); //make first char uppercase
 			
-			dogPrintZone.innerHTML += `
-				<h4 class=typeOfFood">${currentFoodType}</h4>
-			`
-			for(let k = 0; k < dogFoods.dog_brands[i].types[j].volumes.length; k++){//third loop to print pricing and other info
-				var volName = dogFoods.dog_brands[i].types[j].volumes[k].name;
-				var volPrice = dogFoods.dog_brands[i].types[j].volumes[k].price;
-				dogPrintZone.innerHTML += `
-					<p class="foodVolume">${volName}</p>
-					<p class="priceForVolume">$${volPrice}</p>
-				`
+			
+			PrintZone.innerHTML += `
+				<h4 class=foodForBreed">${currentFoodBreeds}</h4>`;
 			}
 		}
-	}
-}
 
-function printCatFood () { //print the cat food object need second function because of breeds. 
-	var catPrintZone = document.getElementById("catFood");
-	
-	for(let i = 0; i < catFoods.cat_brands.length; i++)	{//main loop for printing head info
-		var brandName = catFoods.cat_brands[i].name;
-		catPrintZone.innerHTML += `
-		<h1> Cat Food</h1>
-		<h2 class="brandName">${brandName}</h2>
-		<h3>Breeds, Types, and Pricing</h3>
-		`
-		for(let j = 0; j < catFoods.cat_brands[i].breeds.length; j++){//second loop to print breeds of cats
-		var currentFoodBreeds = catFoods.cat_brands[i].breeds[j];
-		currentFoodBreeds = currentFoodBreeds.replace(/_/, ' '); //replace underscore with space
+		PrintZone.innerHTML += `<h3>Types and Pricing</h3>`;
 
-		currentFoodBreeds = currentFoodBreeds.charAt(0).toUpperCase() + currentFoodBreeds.slice(1); //make first char uppercase
-		
-		
-		catPrintZone.innerHTML += `
-			<h4 class=foodForBreed">${currentFoodBreeds}</h4>
-		`
-		}
-		for(let l = 0; l < catFoods.cat_brands[i].types.length; l++ ){ //print the food types
-			var currentFoodType = catFoods.cat_brands[i].types[l].type;
+		for(let l = 0; l < Foods.brands[i].types.length; l++ ){ //print the food types
+			var currentFoodType = Foods.brands[i].types[l].type;
 			currentFoodType = currentFoodType.replace(/_/, ' '); //replace underscore with space
 
 			currentFoodType = currentFoodType.charAt(0).toUpperCase() + currentFoodType.slice(1); //make first char uppercase
 
-			catPrintZone.innerHTML += `
+			PrintZone.innerHTML += `
 				<h4 class=typeOfFood">${currentFoodType}</h4>
 			`
-			for(let k = 0; k < catFoods.cat_brands[i].types[l].volumes.length; k++){//third loop to print pricing and other info
-				var volName = catFoods.cat_brands[i].types[l].volumes[k].name;
-				var volPrice = catFoods.cat_brands[i].types[l].volumes[k].price;
-				catPrintZone.innerHTML += `
+			for(let k = 0; k < Foods.brands[i].types[l].volumes.length; k++){//third loop to print pricing and other info
+				var volName = Foods.brands[i].types[l].volumes[k].name;
+				var volPrice = Foods.brands[i].types[l].volumes[k].price;
+				PrintZone.innerHTML += `
 					<p class="foodVolume">${volName}</p>
 					<p class="priceForVolume">$${volPrice}</p>
 				`
@@ -86,7 +54,7 @@ function printCatFood () { //print the cat food object need second function beca
 	}
 }
 
-requestDogFood.addEventListener("load", loadFoodRequest); //add a load listener to the requestcatFood 
+requestDogFood.addEventListener("load", loadFoodRequest); //add a load listener to the requestFood 
 requestCatFood.addEventListener("load", loadFoodRequest);
 
 requestDogFood.open("GET", "../json/dogFoods.json"); //give information on retrieving the data to foodRequest
